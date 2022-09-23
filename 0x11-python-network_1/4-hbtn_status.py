bad9d80faad7a8efc0496d177901eb02413139b7
mb3ch3 <33735505+mb3ch3@users.noreply.github.com>
2022-09-23 20:48:34 +0300

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-"""Fetches the URL: https://intranet.hbtn.io/status
-with `requests` module
-"""
-
-import requests
+"""Fetches https://intranet.hbtn.io/status"""
 
 
 if __name__ == "__main__":
-    req = requests.get('https://intranet.hbtn.io/status')
+    import requests
+
+    r = requests.get('https://intranet.hbtn.io/status')
 
     print('Body response:')
-    print('\t- type: {_type}'.format(_type=type(req.text)))
-    print('\t- content: {_content}'.format(_content=req.text))
+    print('\t- type: {}'.format(type(r.text)))
+    print('\t- content: {}'.format(r.text))
